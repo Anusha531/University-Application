366aba57f156b030746ccb7515b12f605884ef49
Anusha Penumalli <anusha531@gmail.com>
2018-12-21 00:41:00 -0600

    People search

---
+++ C#/ContosoUniversity/Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿using ContosoUniversity.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+        // GET: Person
+        public ActionResult Index()
+        {
+            var search = new PeopleSearch();
+            search.People = db.People.Take(search.PageSize);            
+            return View(search);
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Index(PeopleSearch model)
+        {
+            // Get Leases
+            if (model.Empty)
+            {
+                model.People = db.People.Take(model.PageSize);
+            }
+            else
+            {
+                model.People = db.People
+                   .Where(
+                       x =>
+                       (String.IsNullOrEmpty(model.PersonID) || x.ID.ToString().Equals(model.PersonID.Trim()))
+                       && (String.IsNullOrEmpty(model.Name) || x.LastName.ToLower().Trim().Contains(model.Name.ToLower().Trim()) || x.FirstMidName.ToLower().Trim().Contains(model.Name.ToLower().Trim()))
+                       //&& (String.IsNullOrEmpty(model.Name) || x.FirstMidName.ToLower().Trim().Contains(model.Name.ToLower().Trim()))
+                         ).OrderBy(x => x.ID).ToList();
+            }
+
+
+            return View(model);
+        }
+    }
+}
---
+++ C#/ContosoUniversity/ViewModels/PeopleSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class PeopleSearch
+    {
+        public string PersonID { get; set; }
+        public string Name { get; set; }
+        public string PersonType { get; set; }
+
+        public int PageSize { get { return 10; } }
+
+        public IEnumerable<Person> People { get; set; }
+
+        public bool Empty
+        {
+            get
+            {
+                return (string.IsNullOrWhiteSpace(PersonID) &&
+                        string.IsNullOrWhiteSpace(Name) &&
+                        string.IsNullOrWhiteSpace(PersonType));
+            }
+        }
+
+    }
+}
